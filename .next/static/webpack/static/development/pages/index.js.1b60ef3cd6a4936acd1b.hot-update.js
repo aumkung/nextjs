c5272c662c95cf360ce2af4f466294b752bfb44f
tassanai <tassanai.s@mono.co.th>
2019-10-28 19:55:13 +0700

    first commit

---
+++ .next/static/webpack/static/development/pages/index.js.1b60ef3cd6a4936acd1b.hot-update.js
@@ -0,0 +1,42 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "/home/tassanai/Documents/nextjsTest/pages/index.js";
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+/* harmony default export */ __webpack_exports__["default"] = (Index = function Index() {
+  return __jsx("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 2
+    },
+    __self: this
+  }, __jsx("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 3
+    },
+    __self: this
+  }, "Bobo083"));
+}); // export default Index
+// const Index = () => (
+//     <div>
+//       <p>Hello Next.js</p>
+//     </div>
+//   );
+//   export default Index;
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.1b60ef3cd6a4936acd1b.hot-update.js.map
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+const Index = () => (
+    <div>
+        <Link href="/about">
+            <a>About Page</a>
+        </Link>
+        <p>Bobo083</p>
+    </div>
+)
+
+export default Index
