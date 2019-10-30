2cafef7ff2e0290c04a9099e52a6c0920ccc51ef
Tassanai <amm_25204@hotmail.com>
2019-10-30 22:23:34 +0700

    final learn

--- pages/index.js
+++ pages/index.js
@@ -2,56 +2,110 @@ import Layout from '../components/Layout'
 import Link from 'next/link'
 import fetch from 'isomorphic-unfetch'
 
-// const PostLink = props => (
-//     <li>
-//         {/* <Link href={`/post?title=${props.title}`}>
-//             <a>{props.title}</a>
-//         </Link> */}
-//         <Link href="/p/[id]" as={`/p/${props.id}`}>
-//             <a>{props.id}</a>
-//         </Link>
-//     </li>
-// )
-
-// export default function Blog() {
-//     return (
-//         <Layout>
-//             <h1>My Blog</h1>
-//             <ul>
-//                 <PostLink id="Hello Next.js" />
-//                 <PostLink id="Learn Next.js is awesome" />
-//                 <PostLink id="Deploy apps with Zeit" />
-//             </ul>
-//         </Layout>
-//     )
-// }
-
-const Index = props => (
-    <Layout>
-        <h1>Batman TV Shows</h1>
-        <ul>
-            {props.shows.map(show => (
-                <li key={show.id}>
-                    <Link href="/p/[id]" as={`/p/${show.id}`}>
-                        <a>{show.name}</a>
-                    </Link>
-                </li>
-            ))}
-        </ul>
-    </Layout>
-)
-
-Index.getInitialProps = async function() {
-    const res = await fetch('https://api.tvmaze.com/search/shows?q=batman')
-    const data = await res.json()
-
-    console.log(`Show data fetched. Count: ${data.length}`);
-    // console.log(data)
-
-    return {
-        shows: data.map(entry => entry.show)
-    };
+function getPosts() {
+    return [
+        { id: 'hello-nextjs', title: 'Hello Next.js' },
+        { id: 'learn-nextjs', title: 'Learn Next.js is awesome' },
+        { id: 'deploy-nextjs', title: 'Deploy apps with ZEIT' }
+    ]
 }
 
-export default Index
+const PostLink = ({ post }) => (
+    <li>
+        {/* <Link href={`/post?title=${props.title}`}>
+            <a>{props.title}</a>
+        </Link> */}
+        <Link href="/p/[id]" as={`/p/${post.id}`}>
+            <a>{post.title}</a>
+        </Link>
+        <style jsx>{`
+            li {
+                list-style: none;
+                margin: 5px 0;
+            }
+            a {
+                text-decoration: none;
+                color: blue;
+                font-family: 'Arial;
+            }
+            a:hover {
+                opacity: 0.6;
+            }
+        `}</style>
+    </li>
+)
+
+export default function Blog() {
+    return (
+        <Layout>
+            <h1>My Blog</h1>
+            <ul>
+                {getPosts().map(post => (
+                    <PostLink key={post.id} post={post}/>
+                    // <li key={post.id}>
+                    //     <Link href="/p/[id]" as={`/p/${post.id}`}>
+                    //         <a>{post.title}</a>
+                    //     </Link>
+                    // </li>    
+                ))}
+                {/* <PostLink id="Hello Next.js" />
+                <PostLink id="Learn Next.js is awesome" />
+                <PostLink id="Deploy apps with Zeit" /> */}
+            </ul>
+            <style jsx>{`
+                h1, a {
+                    font-family: 'Arial';
+                }
+
+                ul {
+                    padding: 0;
+                }
+
+                li {
+                    list-style: none;
+                    margin: 5px 0;
+                }
+
+                a {
+                    text-decoration: none;
+                    color: blue;
+                }
+
+                a:hover {
+                    opacity: 0.6;
+                }
+            `}</style>
+        </Layout>
+    )
+}
+
+
+// const Index = props => (
+//     <Layout>
+//         <h1>Batman TV Shows</h1>
+//         <ul>
+//             {props.shows.map(show => (
+//                 <li key={show.id}>
+//                     <Link href="/p/[id]" as={`/p/${show.id}`}>
+//                         <a>{show.name}</a>
+//                     </Link>
+//                 </li>
+//             ))}
+//         </ul>
+//     </Layout>
+// )
+
+// Index.getInitialProps = async function() {
+//     const res = await fetch('https://api.tvmaze.com/search/shows?q=batman')
+//     const data = await res.json()
+
+//     console.log(`Show data fetched. Count: ${data.length}`);
+//     // console.log(data)
+
+//     return {
+//         shows: data.map(entry => entry.show)
+//     };
+// }
+
+// export default Index
 
--- pages/p/[id].js
+++ pages/p/[id].js
@@ -1,26 +1,29 @@
 import { useRouter } from 'next/router';
 import Layout from '../../components/Layout';
 import fetch from 'isomorphic-unfetch'
+import Markdown from 'react-markdown'
 
-const Post = props => (
-    <Layout>
-        <h1>{props.show.name}</h1>
-        <p>{props.show.summary.replace(/<[/]?[pb]>/g, '')}</p>
-        <img src={props.show.image.medium} />
-    </Layout>
-)
+// const Post = props => (
+//     <Layout>
+//         <h1>{props.id}</h1>
+//         {/* <h1>{props.show.name}</h1>
+//         <p>{props.show.summary.replace(/<[/]?[pb]>/g, '')}</p>
+//         <img src={props.show.image.medium} /> */}
+//     </Layout>
+// )
 
-Post.getInitialProps = async function(context) {
-    const { id } = context.query
-    const res = await fetch(`https://api.tvmaze.com/shows/${id}`)
-    const show = await res.json()
+// Post.getInitialProps = async function(context) {
+//     const { id } = context.query
+// //     const res = await fetch(`https://api.tvmaze.com/shows/${id}`)
+// //     const show = await res.json()
 
-    console.log(`Fetchd show: ${show.name}`)
+// //     console.log(`Fetchd show: ${show.name}`)
 
-    return { show }
-}
+// //     return { show }
+//     return {id}
+// }
 
-export default Post
+// export default Post
 
 // export default function Post() {
 //   const router = useRouter();
@@ -32,3 +35,47 @@ export default Post
 //     </Layout>
 //   );
 // }
+
+export default () => {
+    const router = useRouter()
+    return (
+        <Layout>
+            <h1>{router.query.id}</h1>
+            <div className="markdown">
+                <Markdown 
+                    source={`
+                        This is our blog post.
+                        Yes. We can have a [link](/link).
+                        And we can have a title as well.
+
+                        ### This is a title
+
+                        And here's the content.
+
+                        is ${router.query.id}
+                    `}
+                />
+            </div>
+            <style jsx global>{`
+                .markdown {
+                    font-family: 'Arial';
+                }
+        
+                .markdown a {
+                    text-decoration: none;
+                    color: blue;
+                }
+        
+                .markdown a:hover {
+                    opacity: 0.6;
+                }
+        
+                .markdown h3 {
+                    margin: 0;
+                    padding: 0;
+                    text-transform: uppercase;
+                }
+            `}</style>
+        </Layout>
+    )
+}
